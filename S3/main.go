f555755b76d5571df2f36f156e7f1f0b015ea8d4
Kyle Riley <kyle.riley@virginpulse.com>
2020-05-12 11:38:48 -0400

    Programmatic access cleanup

--- S3/main.go
+++ S3/main.go
@@ -3,5 +3,11 @@ package main
 func main() {
 	svc := createSession()
 
+	createBucket(svc, "se377krileyprogrammaticaccess", "us-east-1")
+
 	listBucket(svc)
+
+	createObject(svc, "se377krileyprogrammaticaccess", "test.txt")
+
+	removeObject(svc, "se377krileyprogrammaticaccess", "test.txt")
 }
--- S3/programmaticAccess.go
+++ S3/programmaticAccess.go
@@ -6,14 +6,16 @@ import (
 	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 func createSession() *s3.S3 {
 	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String("us-east-1")},
-	)
+		Region:      aws.String("us-east-1"),
+		Credentials: credentials.NewSharedCredentials("", "personal"),
+	})
 
 	if err != nil {
 		exitErrorf("Unable to connect to region")
